75a7e125dc723e4ec67ac81a09c23c1493743841
Elizaveta Taran <lizataran103@gmail.com>
2024-03-21 21:33:27 +0200

    added api to work with json

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-import { getAllTeachers } from "./teachers/index.js";
+import {
+  addTeacher,
+  getAllTeachers,
+  getTeacherById,
+  updateTeacherById,
+  deleteById,
+} from "./teachers/index.js";
 
-const invokeAction = async ({ action }) => {
+const invokeAction = async ({ action, id, name, surname }) => {
   switch (action) {
     case "list":
       const teachers = await getAllTeachers();
-      console.log(teachers);
-      break;
+      return console.log(teachers);
+    case "getById":
+      const teacher = await getTeacherById(id);
+      return console.log(teacher);
+    case "add":
+      const newTeacher = await addTeacher(name, surname);
+      return console.log(newTeacher);
+    case "updateById":
+      const updatedTeacher = await updateTeacherById(id, { name, surname });
+      return console.log(updatedTeacher);
+    case "deleteById":
+      const deletedTeacher = await deleteById(id);
+      return console.log(deletedTeacher);
     default:
       console.log("Unknown action");
   }
 };
 
-invokeAction({ action: "list" });
+invokeAction({ action: "deleteById", id: "LRqVcCY-MWOxrp5OVmRWC" });
--- teachers/index.js
+++ teachers/index.js
@@ -1,9 +1,55 @@
 import fs from "fs/promises";
 import path from "path";
+import { nanoid } from "nanoid";
 
 const teachersPath = path.resolve("teachers", "teachers.json");
+const updateTeachers = (teachers) =>
+  fs.writeFile(teachersPath, JSON.stringify(teachers, null, 2));
 
 export const getAllTeachers = async () => {
   const result = await fs.readFile(teachersPath, "utf-8");
   return JSON.parse(result);
+};
+
+export const getTeacherById = async (id) => {
+  const teachers = await getAllTeachers();
+  const result = teachers.find((teacher) => teacher.id === id);
+  return result || null;
+};
+
+export const addTeacher = async (name, surname) => {
+  const teachers = await getAllTeachers();
+  const newTeacher = {
+    id: nanoid(),
+    name,
+    surname,
+  };
+
+  teachers.push(newTeacher);
+
+  await updateTeachers(teachers);
+
+  return newTeacher;
+};
+
+export const updateTeacherById = async (id, data) => {
+  const teachers = await getAllTeachers();
+  const index = teachers.findIndex((teacher) => teacher.id === id);
+  if (index === -1) {
+    return null;
+  }
+  teachers[index] = { id, ...data };
+  await updateTeachers(teachers);
+  return teachers[index];
+};
+
+export const deleteById = async (id) => {
+  const teachers = await getAllTeachers();
+  const index = teachers.findIndex((teacher) => teacher.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = teachers.splice(index, 1);
+  await updateTeachers(teachers);
+  return result;
 };
