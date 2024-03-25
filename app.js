2c05cc2530b842353505e412fdf85edbec645038
Elizaveta Taran <lizataran103@gmail.com>
2024-03-25 18:56:57 +0200

    express server init

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-import { program } from "commander";
-import {
-  addTeacher,
-  getAllTeachers,
-  getTeacherById,
-  updateTeacherById,
-  deleteById,
-} from "./teachers/index.js";
+import express, { request } from "express";
 
-const invokeAction = async ({ action, id, name, surname }) => {
-  switch (action) {
-    case "list":
-      const teachers = await getAllTeachers();
-      return console.log(teachers);
-    case "getById":
-      const teacher = await getTeacherById(id);
-      return console.log(teacher);
-    case "add":
-      const newTeacher = await addTeacher(name, surname);
-      return console.log(newTeacher);
-    case "updateById":
-      const updatedTeacher = await updateTeacherById(id, { name, surname });
-      return console.log(updatedTeacher);
-    case "deleteById":
-      const deletedTeacher = await deleteById(id);
-      return console.log(deletedTeacher);
-    default:
-      console.log("Unknown action");
-  }
-};
+const app = express();
 
-program
-  .option("-a, --action <type>")
-  .option("-i, --id <id>")
-  .option("-n, --name <name>")
-  .option("-s, --surname <surname>");
-
-program.parse();
-
-invokeAction(program.opts());
+app.listen(3000, () => console.log("Server running on 3000 PORT"));
