4fc6e1fd8baf4c7bce117bc8a6fd6eaef00807cb
Elizaveta Taran <lizataran103@gmail.com>
2024-04-12 15:41:04 +0300

    add decorator ctrlWrapper

--- controllers/teachers-controller.js
+++ controllers/teachers-controller.js
@@ -1,76 +1,57 @@
 import teacherService from '../models/teachers/index.js';
 import HttpError from '../helpers/HttpError.js';
 import teacherSchemas from '../schemas/teacher-schemas.js';
+import { ctrlWrapper } from '../decorators/index.js';
 
-const getAll = async (req, res, next) => {
-  try {
-    const result = await teacherService.getAllTeachers();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const result = await teacherService.getAllTeachers();
+  res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { teacherId } = req.params;
-    const result = await teacherService.getTeacherById(teacherId);
-    if (!result) {
-      throw HttpError(404, `Teacher with ${teacherId} is not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getById = async (req, res) => {
+  const { teacherId } = req.params;
+  const result = await teacherService.getTeacherById(teacherId);
+  if (!result) {
+    throw HttpError(404, `Teacher with ${teacherId} is not found`);
   }
+  res.json(result);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = teacherSchemas.addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await teacherService.addTeacher(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const add = async (req, res) => {
+  const { error } = teacherSchemas.addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await teacherService.addTeacher(req.body);
+  res.status(201).json(result);
 };
 
-const updateById = async (req, res, next) => {
-  try {
-    const { error } = teacherSchemas.updateSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { teacherId } = req.params;
-    const result = await teacherService.updateTeacherById(teacherId, req.body);
-    if (!result) {
-      throw HttpError(404, `Teacher with ${teacherId} is not found`);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateById = async (req, res) => {
+  const { error } = teacherSchemas.updateSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const { teacherId } = req.params;
+  const result = await teacherService.updateTeacherById(teacherId, req.body);
+  if (!result) {
+    throw HttpError(404, `Teacher with ${teacherId} is not found`);
+  }
+  res.json(result);
 };
 
-const deleteById = async (req, res, next) => {
-  try {
-    const { teacherId } = req.params;
-    const result = await teacherService.deleteTeacherById(teacherId);
-    if (!result) {
-      throw HttpError(404, `Teacher with ${teacherId} is not found`);
-    }
-    res.status(204).send();
-  } catch (error) {
-    next(error);
+const deleteById = async (req, res) => {
+  const { teacherId } = req.params;
+  const result = await teacherService.deleteTeacherById(teacherId);
+  if (!result) {
+    throw HttpError(404, `Teacher with ${teacherId} is not found`);
   }
+  res.status(204).send();
 };
 
 export default {
-  getAll,
-  getById,
-  add,
-  updateById,
-  deleteById,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
 };
