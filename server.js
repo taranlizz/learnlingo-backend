ef2736184cc554f8b16cf74c8e169949ee073c2e
Elizaveta Taran <lizataran103@gmail.com>
2024-04-22 15:05:16 +0300

    added PORT env

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import app from './app.js';
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log('Successfully connected to the database');
-    app.listen(3000, () => console.log('Server running on 3000 PORT'));
+    app.listen(PORT, () => console.log(`Server running on ${PORT} PORT`));
   })
   .catch(error => {
     console.log(error.message);
