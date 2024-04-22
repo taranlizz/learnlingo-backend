38826bfd065aa5c7b9ca79abc47f1489868d19f5
Elizaveta Taran <lizataran103@gmail.com>
2024-04-22 15:01:22 +0300

    added connection to mongodb

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-import app from './app.js'
+import mongoose from 'mongoose';
+import app from './app.js';
 
-app.listen(3000, () => console.log('Server running on 3000 PORT'))
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Successfully connected to the database');
+    app.listen(3000, () => console.log('Server running on 3000 PORT'));
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
