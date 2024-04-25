a6c183c33042804bb75b7ce088e7c20cb4e51d6c
Elizaveta Taran <lizataran103@gmail.com>
2024-04-25 22:02:09 +0300

    added schema for teacher model

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,18 +1,60 @@
 import { Schema, model } from 'mongoose';
+import { handleSaveError } from './hooks.js';
 
-const teacherSchema = new Schema({
-    name: String,
-    surname: String,
-    languages: [String],
-    levels: [String],
-    rating: Number,
-    reviews: [{ reviewer_name: String, reviewer_rating: Number, comment: String }],
-    price_per_hour: Number,
-    lessons_done: Number,
-    lesson_info: String,
-    conditions: [String],
-    experience: String,
-});
+const teacherSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    languages: {
+      type: [String],
+      required: true,
+    },
+    levels: {
+      type: [String],
+      enum: [
+        'A1 Beginner',
+        'A2 Elementary',
+        'B1 Intermediate',
+        'B2 Upper-Intermediate',
+        'C1 Advanced',
+      ],
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    reviews: {
+      type: [{ reviewer_name: String, reviewer_rating: Number, comment: String }],
+    },
+    price_per_hour: {
+      type: Number,
+      required: true,
+    },
+    lessons_done: {
+      type: Number,
+      required: true,
+    },
+    lesson_info: {
+      type: String,
+      required: true,
+    },
+    conditions: { type: [String], required: true },
+    experience: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+teacherSchema.post('save', handleSaveError);
 
 const Teacher = model('teacher', teacherSchema);
 
