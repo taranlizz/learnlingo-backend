9523259ccf4655fd2d4d6bb573c6e83559841f1f
Elizaveta Taran <lizataran103@gmail.com>
2024-04-26 16:20:32 +0300

    updated teacherValidationSchema

---
+++ schemas/ErrorMessage.js
@@ -0,0 +1,15 @@
+const ErrorMessage = fieldName => {
+  return {
+    'string.base': `${fieldName} should be a type of 'string'`,
+    'string.min': `${fieldName} should have a minimum length of 2`,
+    'array.base': `${fieldName} should be a type of 'array'`,
+    'array.includes': `items in ${fieldName} array should be a type of 'string'`,
+    'array.includesRequiredUnknowns': `missing required items in ${fieldName} array`,
+    'number.base': `${fieldName} should be a type of 'number'`,
+    'number.min': `${fieldName} field's value less than the allowed minimum value`,
+    'any.only': `items in ${fieldName} array should match list of valid values`,
+    'any.required': `missing required ${fieldName} field`,
+  };
+};
+
+export default ErrorMessage;
--- schemas/teacher-schemas.js
+++ schemas/teacher-schemas.js
@@ -1,16 +1,36 @@
 import Joi from 'joi';
 
-const add = Joi.object({
-  name: Joi.string().required(),
-  surname: Joi.string().required(),
+import ErrorMessage from './ErrorMessage.js';
+
+const teacherValidationSchema = Joi.object({
+  name: Joi.string().min(2).required().messages(ErrorMessage('name')),
+  surname: Joi.string().required().messages(ErrorMessage('surname')),
+  languages: Joi.array()
+    .items(Joi.string().required())
+    .required()
+    .messages(ErrorMessage('languages')),
+  levels: Joi.array()
+    .items(
+      Joi.string()
+        .required()
+        .valid(
+          'A1 Beginner',
+          'A2 Elementary',
+          'B1 Intermediate',
+          'B2 Upper-Intermediate',
+          'C1 Advanced'
+        )
+    )
+    .required()
+    .messages(ErrorMessage('levels')),
+  pricePerHour: Joi.number().required().min(0).messages(ErrorMessage('pricePerHour')),
+  lessonsDone: Joi.number().min(0).required().messages(ErrorMessage('lessonsDone')),
+  lessonInfo: Joi.string().required().messages(ErrorMessage('lessonInfo')),
+  conditions: Joi.array()
+    .items(Joi.string().required())
+    .required()
+    .messages(ErrorMessage('conditions')),
+  experience: Joi.string().required().messages(ErrorMessage('experience')),
 });
 
-const update = Joi.object({
-  name: Joi.string(),
-  surname: Joi.string(),
-});
-
-export default {
-  add,
-  update,
-};
+export default teacherValidationSchema;
