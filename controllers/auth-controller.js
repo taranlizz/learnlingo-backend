e6e4f8530e62e610d3b28ea5d15308460bab60b0
Elizaveta Taran <lizataran103@gmail.com>
2024-05-01 18:23:55 +0300

    added signin controller + jwt

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,11 +1,51 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User.js';
 
 import { ctrlWrapper } from '../decorators/index.js';
 
 import { HttpError } from '../helpers/index.js';
 
-const signup = async (res, req) => {};
+const { JWT_SECRET } = process.env;
+
+const signUp = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, 'User with this email already exists');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashedPassword });
+
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+    surname: newUser.surname,
+  });
+};
+
+const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, `User with email ${email} is not found`);
+  }
+
+  const doesPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!doesPasswordMatch) {
+    throw HttpError(401, `Incorrect password`);
+  }
+
+  const payload = { id: user.id };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '23h' });
+
+  res.json({ token });
+};
 
 export default {
-  signup: ctrlWrapper(signup),
+  signUp: ctrlWrapper(signUp),
+  signIn: ctrlWrapper(signIn),
 };
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,6 +2,15 @@ import express from 'express';
 
 import authController from '../../controllers/auth-controller.js';
 
+import { isEmptyBody } from '../../middlewars/index.js';
+import { validateBody } from '../../decorators/index.js';
+
+import userSchemas from '../../schemas/user-schemas.js';
+
 const authRouter = express.Router();
+
+authRouter.post('/signup', isEmptyBody, validateBody(userSchemas.signUp), authController.signUp);
+
+authRouter.post('/signin', isEmptyBody, validateBody(userSchemas.signIn), authController.signIn);
 
 export default authRouter;
